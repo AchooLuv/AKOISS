4460c7bc85a6c88a3b85ef82ee5fa3431152cc5e
shie <ShieTsun@outlook.com>
2025-05-03 15:55:10 +0800

    修复错误

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      sass: {
-        api: 'modern',
-      },
-    },
-  },
   plugins: [vue()],
   resolve: {
     alias: {
