1b3663bb1328e43e650a0593a0ac929c94b2512b
shie <ShieTsun@outlook.com>
2025-05-03 13:33:57 +0800

    更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      sass: {
+        api: 'modern',
+      },
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
